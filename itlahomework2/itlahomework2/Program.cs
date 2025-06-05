4a82d619de97a9602595687b8ea4fffcad48aff4
Madelin <madelinsanchezcuevas2006@gmail.com>
2025-06-04 21:12:43 -0400

    subiendo tarea

---
+++ itlahomework2/itlahomework2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Madelin Sanchez Cuevas
+//20242511
+
+namespace Numero_par_o_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            Console.Write("Escribe un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+            }
+            else
+            {
+                Console.WriteLine("El numero es impar");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
