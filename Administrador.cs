8e953cd97d74d85c38ae1846a7dc1be08006e560
Madelin <madelinsanchezcuevas2006@gmail.com>
2025-09-26 23:33:15 -0400

    tarea1

---
+++ Administrador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MAPA_DE_CLASES.Models
+{
+    public class Administrador: Docente
+    {
+        public Administrador(int id, string nombre, string apellido,
+                             DateTime fechaIngreso, DateTime fechaContratacion, decimal salario,
+                             string departamento, string materia)
+            :base(id, nombre, apellido, fechaIngreso, fechaContratacion, salario, departamento, materia)
+        {
+        }
+
+
+    }
+}
