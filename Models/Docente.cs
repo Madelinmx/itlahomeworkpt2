bddb6c08d19835df074395c7a24ee4d9690acdda
Madelin <madelinsanchezcuevas2006@gmail.com>
2025-09-26 22:52:09 -0400

    tarea1

---
+++ Models/Docente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MAPA_DE_CLASES.Models
+{
+    public class Docente : Empleado
+    {
+        public string Departamento { get; set; }
+        public string Materia { get; set; }
+
+        public Docente(int id, string nombre, string apellido,
+                       DateTime fechaIngreso, DateTime fechaContratacion, decimal salario,
+                       string departamento, string materia)
+            : base(id, nombre, apellido, fechaIngreso, fechaContratacion, salario)
+        {
+            Departamento = departamento;
+            Materia = materia;
+        }
+
+        public override void MostrarInfo()
+        {
+            base.MostrarInfo();
+            Console.WriteLine($"   Departamento: {Departamento}, Materia: {Materia}");
+        }
+    }
+}
---
+++ Models/Empleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MAPA_DE_CLASES.Models
+{
+    public class Empleado : MiembroDeLaComunidad
+    {
+        public DateTime FechaContratacion { get; set; }
+        public decimal Salario { get; set; }
+
+        public Empleado(int id, string nombre, string apellido,
+                        DateTime fechaIngreso, DateTime fechaContratacion, decimal salario)
+            : base(id, nombre, apellido, fechaIngreso)
+        {
+            FechaContratacion = fechaContratacion;
+            Salario = salario;
+        }
+
+        public override void MostrarInfo()
+        {
+            base.MostrarInfo();
+            Console.WriteLine($"   Contratado: {FechaContratacion:dd/MM/yyyy}, Salario: {Salario:C2}");
+        }
+    }
+}
