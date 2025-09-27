043264ae6790a49147447f54112612b662e73ebe
Madelin <madelinsanchezcuevas2006@gmail.com>
2025-09-27 01:29:45 -0400

    Homework1

---
+++ mapa_de_clases1/Models/Administrativo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MAPA_DE_CLASES.Models;
+
+namespace MAPA_DE_CLASES.Models
+{
+    public class Administrativo : Empleado
+    {
+        public string Area { get; set; }
+        public string Puesto { get; set; }
+
+        public Administrativo(int id, string nombre, string apellido,
+                              DateTime fechaIngreso, DateTime fechaContratacion, decimal salario,
+                              string area, string puesto)
+            : base(id, nombre, apellido, fechaIngreso, fechaContratacion, salario)
+        {
+            Area = area;
+            Puesto = puesto;
+        }
+
+        public override void MostrarInfo()
+        {
+            base.MostrarInfo();
+            Console.WriteLine($"   Área: {Area}, Puesto: {Puesto}");
+        }
+    }
+}
