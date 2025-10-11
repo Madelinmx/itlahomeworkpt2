74ffdedcd1d82592da4c35bd2765dd2d82340ae8
Madelin <madelinsanchezcuevas2006@gmail.com>
2025-10-11 14:10:59 -0400

    Add files via upload

---
+++ homeworkmade/homeworkmade/Models/Estudiante.cs
@@ -0,0 +1,10 @@
+﻿namespace MiApi.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; } = string.Empty;
+        public int Edad { get; set; }
+        public string Carrera { get; set; } = string.Empty;
+    }
+}
